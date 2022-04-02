7bf69974ca4f92aacf01cf693710c4c4fd8b0190
Ashish-1016 <ajeswani80@gmail.com>
2022-04-03 02:08:46 +0530

    Updates

---
+++ precache-manifest.e8044395984e3a415bdb8f0a3e01627a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc79673f949a16b37537",
+    "url": "/facebook-clone/static/css/main.eff54fc7.chunk.css"
+  },
+  {
+    "revision": "bc79673f949a16b37537",
+    "url": "/facebook-clone/static/js/main.bc79673f.chunk.js"
+  },
+  {
+    "revision": "462fada37566201ee509",
+    "url": "/facebook-clone/static/js/1.462fada3.chunk.js"
+  },
+  {
+    "revision": "8030039b9e171936fe1a",
+    "url": "/facebook-clone/static/js/2.8030039b.chunk.js"
+  },
+  {
+    "revision": "72e0e6b76ddad1b64681",
+    "url": "/facebook-clone/static/js/runtime~main.72e0e6b7.js"
+  },
+  {
+    "revision": "23dbc0959d676ef8dcfb038c602ef2a1",
+    "url": "/facebook-clone/index.html"
+  }
+];
